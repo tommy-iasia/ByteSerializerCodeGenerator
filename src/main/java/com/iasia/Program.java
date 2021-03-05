1ef73e15044bac57195e25c41e19648052642bbb
tommylee <tommylee@iasia.com.hk>
2021-03-05 16:04:06 +0800

    example

--- src/main/java/com/iasia/Program.java
+++ src/main/java/com/iasia/Program.java
@@ -2,13 +2,17 @@ package com.iasia;
 
 import com.iasia.code.DataClass;
 
-import java.io.IOException;
-
 public class Program {
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public static void main(String[] args) throws IOException {
-        DataClass.write("src\\main\\java\\com\\iasia\\code\\example\\Example1.yaml");
-        DataClass.write("src\\main\\java\\com\\iasia\\code\\example\\Example2.yaml");
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("write " + arg);
+
+            try {
+                DataClass.write(arg);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
